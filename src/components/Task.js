2a0180d49a7cb3a1a22571c45dd62cc2acc817ae
NicolasG <nicolas.grissi@gmail.com>
2022-02-07 17:20:54 +0100

    format code

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,19 +1,19 @@
-import './Task.css'
+import "./Task.css";
 
 const Task = (props) => {
-  const {id,body, checked,onChange} = props
+  const { id, body, checked, onChange } = props;
   return (
     <li key={id}>
       {checked ? <s>{body}</s> : body}{" "}
-    <span>
-      <input
-        checked={checked ? true : false}
-        onChange={() => onChange(id)}
-        type="checkbox"
-      />
-    </span>
-  </li>
-  )
-}
+      <span>
+        <input
+          checked={checked ? true : false}
+          onChange={() => onChange(id)}
+          type="checkbox"
+        />
+      </span>
+    </li>
+  );
+};
 
-export default Task
+export default Task;
